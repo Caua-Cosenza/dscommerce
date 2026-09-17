d60e5dceefe1d11e47092cadc74a9808f0d93391
cauacosenza88-jpg <cauacosenza88@gmail.com>
2026-09-17 18:50:41 -0300

    programa dscommerce, adicionei relacionamento 1 para muitor e muitos para um nas classes order e user, tambem ja implementando enums dos pedidos.

---
+++ dscommerce/src/test/java/com/cosenza/dscommerce/DscommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cosenza.dscommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
