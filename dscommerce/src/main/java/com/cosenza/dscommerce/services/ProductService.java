961245485f588e95fbcf739090301c468a134f50
cauacosenza88-jpg <cauacosenza88@gmail.com>
2026-09-22 09:43:36 -0300

    GET Products paged

--- dscommerce/src/main/java/com/cosenza/dscommerce/services/ProductService.java
+++ dscommerce/src/main/java/com/cosenza/dscommerce/services/ProductService.java
@@ -1,6 +1,8 @@
 package com.cosenza.dscommerce.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -8,21 +10,22 @@ import com.cosenza.dscommerce.dto.ProductDTO;
 import com.cosenza.dscommerce.entities.Product;
 import com.cosenza.dscommerce.repositories.ProductRepository;
 
-
-@Service 
+@Service
 public class ProductService {
 
-    @Autowired 
+    @Autowired
     private ProductRepository repository;
 
-
-    @Transactional (readOnly = true)
+    @Transactional(readOnly = true)
     public ProductDTO findById(Long id) {
-
         Product product = repository.findById(id).get();
         return new ProductDTO(product);
-        
+    }
 
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        Page<Product> result = repository.findAll(pageable);
+        return result.map(x -> new ProductDTO(x));
     }
 
 }
